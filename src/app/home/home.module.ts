75995caf23ad5648edcefe9850ae07a434e87128
LuAnderson <lucasand96@gmail.com>
2018-11-20 22:02:09 -0200

    Componente portfolio :book:

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
 import { ProfessionalComponent } from './professional/professional.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [HomeComponent, MenuComponent, FooterComponent, IntroComponent, AboutComponent, ProfessionalComponent],
+  declarations: [HomeComponent, MenuComponent, FooterComponent, IntroComponent, AboutComponent, ProfessionalComponent, PortfolioComponent],
   imports: [
     CommonModule
   ],
