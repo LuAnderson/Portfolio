ab72247ebaf96d51ba23e644b019332074cbd443
LuAnderson <lucasand96@gmail.com>
2020-02-22 20:21:43 -0300

    feat(*): cria component de header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-[0.0.1] - 2020-02-22 (19h07)
+[0.1.0] - 2020-02-22 
 
 ### Chore
 > 1 = Instala dependentes para o desenvolvimento do projeto
@@ -8,3 +8,7 @@
 > 2 = Organiza distribuição de pastas e rotas 
 - Cria pasta de components/pages/styles
 
+[0.2.0] - 2020-02-22 
+
+### Feature
+> 1 = Cria component de Header
---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from './styles';
+import logo from '../../logo.svg';
+
+function Header() {
+  return(
+    <S.Header className="App-header">
+        <S.Image src={logo} className="App-logo" alt="logo" />
+        <S.Text>
+            Edit <code>src/App.js</code> and save to reload.
+        </S.Text>
+        <S.Anchor
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+        </S.Anchor>
+    </S.Header>
+  );
+}
+
+export default Header;
