e3b308a65dbcd2f0d18baff5c978d33dc07b2c37
LuAnderson <lucasand96@gmail.com>
2020-03-29 20:09:33 -0300

    feat(*): implementa view do componentes na home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
+import { ProfessionalComponent } from './professional/professional.component';
+import { TrainningComponent } from './trainning/trainning.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    IntroComponent,
+    AboutComponent,
+    ProfessionalComponent,
+    TrainningComponent,
+  ],
   imports: [
     CommonModule
   ],
