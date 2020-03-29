313acd4244ddbc4463e614dcb13b0800ab5afb32
LuAnderson <lucasand96@gmail.com>
2020-03-29 19:14:25 -0300

    feat(menu.c): adiciona component de menu

---
+++ src/app/home/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showMobileMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+  }
+
+}
