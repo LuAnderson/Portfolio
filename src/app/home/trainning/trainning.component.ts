f4f82346be78017e2f0384e908d457fbbf554283
LuAnderson <lucasand96@gmail.com>
2020-03-29 19:21:11 -0300

    feat(trainning.c): adiciona component de trainning

---
+++ src/app/home/trainning/trainning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainning',
+  templateUrl: './trainning.component.html',
+  styleUrls: ['./trainning.component.scss']
+})
+export class TrainningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
