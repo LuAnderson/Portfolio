ab22053c44b3a0de00d3f90782f2345ac5f4561d
LuAnderson <lucasand96@gmail.com>
2020-03-29 19:20:18 -0300

    feat(professional.c): adiciona component de professional

---
+++ src/app/home/professional/professional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professional',
+  templateUrl: './professional.component.html',
+  styleUrls: ['./professional.component.scss']
+})
+export class ProfessionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
