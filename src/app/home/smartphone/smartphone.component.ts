9fade1074b460264d7f30193dfe8aa456f38c462
LuAnderson <lucasand96@gmail.com>
2020-03-29 19:47:41 -0300

    feat(smartphone.c): adiciona component de smartphone

---
+++ src/app/home/smartphone/smartphone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smartphone',
+  templateUrl: './smartphone.component.html',
+  styleUrls: ['./smartphone.component.scss']
+})
+export class SmartphoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
