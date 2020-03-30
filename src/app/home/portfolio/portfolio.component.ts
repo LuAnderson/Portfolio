d464bb0959dd1ec0df36f33ded847533cd45360d
LuAnderson <lucasand96@gmail.com>
2020-03-29 22:51:51 -0300

    feat(portfolio.c): adiciona component de portfolio

---
+++ src/app/home/portfolio/portfolio.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    keyboard: true,
+    mousewheel: false,
+    navigation: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: true,
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
