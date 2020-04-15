196462ed2c56682c46d876488559d6b93d4d05c0
LuAnderson <lucasand96@gmail.com>
2020-04-15 01:08:16 -0300

    feat(menu.c): implementa ancoras e efeitos no menu

--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-menu',
@@ -6,15 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  deviceInfo = null;
 
-  constructor() { }
+  constructor(private deviceService: DeviceDetectorService) {
+  }
 
   ngOnInit(): void {
   }
 
   showMobileMenu() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active');
-  }
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    if (this.deviceService.isMobile()) {
+      const navbar = document.getElementById('navbar');
+      navbar.classList.toggle('active');
+    }
 
+    return;
+  }
 }
