7e0c5f5c930de13a93a1d16e4c198bd74b242d8e
LuAnderson <lucasand96@gmail.com>
2020-05-02 16:37:11 -0300

    feat(portfolio.c): remove slide

--- src/app/home/portfolio/portfolio.component.ts
+++ src/app/home/portfolio/portfolio.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
-
-import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-portfolio',
@@ -9,20 +6,8 @@ import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
-  deviceInfo = null;
 
-  public config: SwiperConfigInterface = {
-    direction: 'horizontal',
-    slidesPerView: this.deviceService.isMobile() ? 2 : 3,
-    keyboard: true,
-    mousewheel: false,
-    navigation: true,
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: true,
-  };
-
-  constructor(private deviceService: DeviceDetectorService) { }
+  constructor() { }
 
   ngOnInit() {
   }
